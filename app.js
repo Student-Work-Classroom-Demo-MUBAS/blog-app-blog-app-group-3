7a24de542c52c7cf4bc070f3d7bdcd6b0c68d3ae
DebraGwedeza <debragwedeza@gmail.com>
2025-08-23 23:31:55 +0200

    implemented get route to render edit form with post data

--- app.js
+++ app.js
@@ -1,50 +1,8 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const methodOverride = require("method-override"); //method-override to simulate PUT and DELETE requests because HTML forms don’t support them natively keeping routing RESTful and modular
-
-// In-memory data store
-let posts = [
-   {id: 1, title: "First Post", content: "The first post."},
-   {id: 2, title: "Second Post", content: "The second post."}
-];
-let nextId = 1;
-
-// View engine & middleware
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({ extended: true }));//Enables parsing of form data(req.body)
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride("_method")); //allows PUT and DELETE via query string (_method)
-//renders the lists of posts with edit button
-app.get('/posts', (req, res) => {
-   res.render('views/index', { views});
-});
-
-//edit route
-app.get("/posts/edit/:id", (req, res) => {
- const {id} = req.params;
- const post = posts.find(p => p.id == id);
- res.render("posts/edit", { post }); //passes post to EJS view
-});
-
-//update route
-app.put("/posts/edit/:id", (req, res) => {
- const {id} = req.params;
- const {title, content} =req.body;
- const post = posts.find(p => p.id == id);
- post.title = title;
- post.content = content;
- res.redirect('/post/');
-});
- 
-//Delete route
-app.delete("/posts/:id",(req, res) => {
-   const {id} = req.params;
-   posts = posts.filter(p => p.id != id);
-   res.render("posts/delete-succes", {deletedId: id}); // success view
-});
-
-app.listen(3000, () => {
-   console.log("server running on http://localhost:3000");
+app.get('/post/:id/edit', (req, res) => {
+  const postId = req.params.id;
+  const post = postMessage.find(p => p.id === postId);
+  if (!post) {
+    return res.status(404).render('error', {message: 'Post not found' });
+  }
+  res.render('edit', {post, successMessage: null, errorMessage: null});
 });
